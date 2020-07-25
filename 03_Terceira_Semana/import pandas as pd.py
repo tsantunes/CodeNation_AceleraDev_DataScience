d0de3fa1d8b2deda2e4a5bbb98236e84c1223b35
tsantunes <tsantunes@gmail.com>
2020-07-25 11:20:36 -0300

    atualização

--- 03_Terceira_Semana/import pandas as pd.py
+++ 03_Terceira_Semana/import pandas as pd.py
@@ -1,20 +1,35 @@
-    import pandas as pd
+# !git add "file.py"
+# !git commit -m "My commit"
+# !git push origin master
+
+# Useful commands to work in daily life:
+
+# git checkout -b "branchname" ->  creates new branch
+# git branch                   ->  lists all branches
+# git checkout "branchname"    ->  switches to your branch
+# git push origin "branchname" ->  Pushes to your branch
+# git add */filename           -> Stages *(All files) or by given file name
+# git commit -m "commit message" -> Commits staged files
+# git push                     -> Pushes to your current branch
+
+import pandas as pd
 
 pd.set_option('display.max_columns', 100000)
 pd.set_option('display.max_rows', 100000) 
 pd.set_option('display.max_colwidth', 100000) 
 pd.set_option('display.width', None) 
 
+pd.data
+df = pd.read_csv("datasets_554905_1035602_houses_to_rent.csv")
+df.describe()
+df.shape
+df.columns
+df.info()
 
-    df = pd.read_csv("datasets_554905_1035602_houses_to_rent.csv")
-    df.describe()
-    df.shape
-    df.columns
-    df.info()
-    
-    df.isnull().sum()
-    
-    df.groupby('furniture')['area'].mean()
+df.isnull().sum()
+
+df.groupby('furniture')['area'].mean()
 
 df
 print(df)
+
